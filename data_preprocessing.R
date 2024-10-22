37b1aad39a808a2e43bcf110e27734ed30761229
Shahzaib Khan <ishahzaibkhan@gmail.com>
2024-10-23 00:26:46 +0500

    add clean_text function

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -3,31 +3,38 @@ library(skimr)
 library(tm)
 library(SnowballC)
 
+# Function to clean text data
+clean_text <- function(text_vector) {
+  cleaned_text <- sapply(text_vector, function(text) {
+    text <- tolower(text) # Converting to Lowercase
+    text <- str_remove_all(text, "<[^>]+>") # Removing HTML syntax using regex
+    text <- removeWords(text, stopwords("en")) # Removing stopwords
+    text <- gsub("[[:punct:]]+", " ", text) # Removing punctuation using regex
+    text <- wordStem(text, "en") # Stemming
+    text <- removeNumbers(text) # Removing Numbers
+    # Removing emojis using regex
+    text <- gsub("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF]", "", text, perl = TRUE)
+    text <- gsub("\\b\\w{1,2}\\b", "", text) # Removing words with up to 2 letters that have no impact especially after stemming
+    text <- gsub("\\s+", " ", text) # Removing excess spaces
+    text <- trimws(text) # Removing the spacing before the text starts/ends
+    
+    return(text) 
+  })
+  
+  return(cleaned_text)
+}
+
 df <- read.csv("./IMDB Dataset.csv")
 
 # Understanding the data and evaluating problems
 skim(df)
 head(df)
 View(df)
+
+# Taking sample from the 50k rows
 sample_df <- df %>%
   sample_n(100)
-print(sample_df$review)
 
-
-data_preprocess <- function(text) {
-  
-}
-# Performing the operations on smaller scle
-sample_df$review <- sapply(sample_df$review, function(text) {
-  text <- tolower(text)
-  text <- str_remove_all(text, "<[^>]+>")
-  text <- removeWords(text, stopwords("en"))
-  text <- gsub("[[:punct:]]+", " ", text)
-  text <- wordStem(text,"en")
-  text <- removeNumbers(text)
-  text <- gsub("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF]", "", text, perl = TRUE)
-  text <- gsub("\\b\\w{1,2}\\b", "", text)
-  text <- gsub("\\s+", " ", text)
-  text <- trimws(text)
-})
+# Performing the operations on smaller scale
+sample_df$review <- clean_text(sample_df$review)
 
