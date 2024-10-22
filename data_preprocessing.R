b5581a0b765ee3e7179531b70473cd4ba11cb0fe
Shahzaib Khan <ishahzaibkhan@gmail.com>
2024-10-22 22:32:41 +0500

    convert to lowercase

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -2,3 +2,20 @@ library(tidyverse)
 library(skimr)
 
 df <- read.csv("./IMDB Dataset.csv")
+
+# Understanding the data and evaluating problems
+skim(df)
+head(df)
+View(df)
+sample_df <- df %>%
+  sample_n(10)
+print(sample_df$review)
+
+# Problems (Review Column)
+# Mixed case i.e upper and lower case
+# Html tags/symbols
+# Punctuation
+
+# Converting into lowercase
+df$review <- tolower(df$review)
+
